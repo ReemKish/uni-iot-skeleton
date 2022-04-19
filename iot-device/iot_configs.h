dfaccdb5835dff064376e481764c3e69f1d68a21
davidmolina <dmolina@deliveright.com>
2022-04-19 21:02:57 +0300

    IoT Device first part
    Just send a telemetry, still not saving the message anywhere.

---
+++ iot-device/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "HOTBOX-713E"
+#define IOT_CONFIG_WIFI_PASSWORD "0525161127"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "SkeletonH.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "SkeletonESP32"
+#define IOT_CONFIG_DEVICE_KEY "YRYbJ61ba+qKvPazEYYEGW+6KtqHn09AYGD0WOxmHx8="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
